378b284cf9c9932578b5df654b45f692f34a12de
Jason Toups <me@jasontoups.com>
2022-05-12 12:18:51 -0700

    completes brute force solution for binary search

--- binary-search/app.js
+++ binary-search/app.js
@@ -26,11 +26,66 @@ TODO
 - setup binary search solution
 */
 
+/* Initial Setup
+This would be a good function scaffold to use for your interviews, giving the candidates a place to start that's easy to understand and work with.
+
 function closestNumber (num, array) {
   let index = 0;
   let winningIndex = -1;
 
   while (index < array.length) {
+    index++;
+  }
+
+  if (winningIndex === -1) {
+    return -1
+  } else {
+    return array[winningIndex];
+  }
+};
+*/
+
+/* Brute Force Solution:
+For an initial brute force solution, we can simply loop through the array and compare each number to the prize value.
+The runtime of this is O(n).
+
+function closestNumber (num, array) {
+  let index = 0;
+  let winningIndex = -1;
+  let difference = num;
+  
+  while (index <= array.length) {
+    
+    if ((num - array[index]) <= difference && (num - array[index]) >= 0) {
+      difference = (num - array[index]);
+      winningIndex = index;
+    }
+    index++;
+  }
+  
+  if (winningIndex === -1) {
+    return -1
+  } else {
+    return array[winningIndex];
+  }
+};
+*/
+/* Binary Search Solution:
+For a binary search solution, we can use a binary search to find the closest number to the prize value.
+
+This will improve runtime by a factor of O(log n).
+*/
+function closestNumber (num, array) {
+  let index = 0;
+  let winningIndex = -1;
+  let difference = num;
+
+  while (index <= array.length) {
+    
+    if ((num - array[index]) <= difference && (num - array[index]) >= 0) {
+      difference = (num - array[index]);
+      winningIndex = index;
+    }
     index++;
   }
 
@@ -46,7 +101,7 @@ var bidsArray = [2, 7, 11, 15];
 console.log(closestNumber(prize, bidsArray));
 
 var prize = 11;
-var bidsArray = [15, 2, 11, 7];
+var bidsArray = [15, 2, 11, 7, 10];
 console.log(closestNumber(prize, bidsArray));
 
 var prize = 31;
