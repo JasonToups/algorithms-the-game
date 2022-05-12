dbf639889e01d6807b1a69cca940107ec5b32f0f
Jason Toups <me@jasontoups.com>
2022-05-12 13:38:24 -0700

    finishes binary search solution

--- binary-search/app.js
+++ binary-search/app.js
@@ -74,19 +74,39 @@ function closestNumber (num, array) {
 For a binary search solution, we can use a binary search to find the closest number to the prize value.
 
 This will improve runtime by a factor of O(log n).
+
 */
 function closestNumber (num, array) {
-  let index = 0;
   let winningIndex = -1;
   let difference = num;
+  // we need to sort the array to use binary search
+  array = array.sort((a, b) => a - b);
+  
+  // we need a 2 pointer approach to track the start & end conditions.
+  let start = 0;
+  let end = array.length - 1;
 
-  while (index <= array.length) {
-    
-    if ((num - array[index]) <= difference && (num - array[index]) >= 0) {
-      difference = (num - array[index]);
-      winningIndex = index;
+  // Three things need to happen here:
+  // We need to check if the middle index is the winning index.
+  // If it is, then return the winning index.
+  // If it is less than the winning index, then we need to set the index to half of the index & the length of the array.
+  // If it is more than the winning index, then we need to set the index to half of the index.
+  while (start<=end) {
+    // To perform the binary search, we need to find the middle index of the array, for every cycle of the loop.
+    // Use Math.floor() to round down to the nearest whole number.
+    let mid = Math.floor((start + end) / 2);
+
+    if (array[mid] === num) {
+      return array[mid];
+    } else if (array[mid] < num) {
+      if ((num - array[mid]) <= difference && (num - array[mid]) >= 0) {
+        difference = (num - array[mid]);
+        winningIndex = mid;
+      } 
+      start = mid + 1;
+    } else {
+      end = mid - 1;
     }
-    index++;
   }
 
   if (winningIndex === -1) {
